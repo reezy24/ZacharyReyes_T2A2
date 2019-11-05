52456d05e2f2b39ef7264e268ee101976f1df42e
reez <zreye24@gmail.com>
2019-11-05 13:02:57 +1000

    write first acceptance test

---
+++ src/game_dev_app/db/migrate/20191105012043_create_projects.rb
@@ -0,0 +1,12 @@
+class CreateProjects < ActiveRecord::Migration[5.2]
+  def change
+    create_table :projects do |t|
+      t.string :title, null: false
+      t.text :description, null: false
+      t.string :duration, null: false
+      t.integer :budget, null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ src/game_dev_app/spec/features/create_a_project_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe 'creating a project' do
+    it 'saves and displays the resulting project' do
+        
+        visit '/projects/new'
+
+        # fill project details
+        fill_in 'Title', with: 'Call of Duty'
+        fill_in 'Description', with: 'The best FPS ever!'
+        fill_in 'Duration', with: '6 months'
+        fill_in 'Budget', with: '$50000'
+
+        # add images
+        attach_file 'Images', 'app/assets/images/local/cod.jpg'
+
+        # add role
+        select 'Animator', from: 'Roles'
+        choose 'Blender'
+        choose 'Cinema3D'
+        fill_in 'Role Description', with: 'I need an Animator.'
+        click_button 'Add Role'
+
+        click_button 'Post Project'
+
+        # expectations
+        expect(page).to have_content('Call of Duty')
+        expect(page).to have_content('The best FPS ever!')
+        expect(page).to have_content('6 months')
+        expect(page).to have_content('$50000')
+        # missing: expect image
+        expect(page).to have_content('Animator')
+        expect(page).to have_content('Blender')
+        expect(page).to have_content('Cinema3D')
+        expect(page).to have_content('I need an Animator.')
+
+    end
+end
