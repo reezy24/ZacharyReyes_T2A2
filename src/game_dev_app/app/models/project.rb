bb6b153e44e764b7accb46726a4abc40969bbcf1
reez <zreye24@gmail.com>
2019-11-06 16:30:11 +1000

    tests, write associations in associations.txt

--- src/game_dev_app/app/models/project.rb
+++ src/game_dev_app/app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :owner, :class_name => 'Member'
-
+  has_many :project_roles, dependent: :destroy
   validates :owner, :title, :description, :duration, :budget, presence: true
   validates :budget, numericality: { only_integer: true }
 end
