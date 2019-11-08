7f1e043540db46a6442578fe1751de99d32de321
reez <zreye24@gmail.com>
2019-11-08 10:04:50 +1000

    queries for my dashboard - projects

--- src/game_dev_app/app/controllers/my_dashboard_controller.rb
+++ src/game_dev_app/app/controllers/my_dashboard_controller.rb
@@ -1,5 +1,20 @@
 class MyDashboardController < ApplicationController
   def projects
+
+    # get all owned projects where owner matches current member
+    @owned_projects = Project.where(owner: current_member)
+    
+    # get all involved projects by first finding all of the 
+    # member's project roles and then matching the project id to
+    # the corresponding project
+    @involved_projects = []
+    project_roles = ProjectRole.where(member_id: current_member.id)
+    project_roles.each do |pr|
+      @involved_projects << Project.find(pr.project_id)
+    end
+    p "member id: #{current_member.id}"
+    @projects = @owned_projects + @involved_projects
+
   end
 
   def offers
