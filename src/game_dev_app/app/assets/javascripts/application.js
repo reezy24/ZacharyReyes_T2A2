ba21ebe46497682cb71b9ce3c4df7b3e16c7c360
reez <zreye24@gmail.com>
2019-11-08 15:45:14 +1000

    jquery fix

--- src/game_dev_app/app/assets/javascripts/application.js
+++ src/game_dev_app/app/assets/javascripts/application.js
@@ -10,8 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require jquery
+//= require jquery_ujs
 //= require activestorage
 //= require turbolinks
+//= require nested_form_fields
 //= require_tree .
-//= require_nested_form_fields
+
+
