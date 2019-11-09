18b3049218fcb4e8e82a0c57cb24cf538f20b8a3
reez <zreye24@gmail.com>
2019-11-10 01:39:54 +1000

    proposals work in production

--- src/game_dev_app/app/controllers/offers_controller.rb
+++ src/game_dev_app/app/controllers/offers_controller.rb
@@ -1,6 +1,8 @@
 class OffersController < ApplicationController
   before_action :set_offer, only: [:show, :edit, :update, :destroy]
   before_action :set_receiver, only: [:new]
+  before_action :set_project_role, only: [:new]
+
   # GET /offers
   # GET /offers.json
   def index
@@ -15,11 +17,14 @@ class OffersController < ApplicationController
   # GET /offers/new
   def new
     @offer = Offer.new
-    @roles = []
-    Project.all.each do |p|
-      if p.owner == current_member then
-        p.project_roles.each do |r|
-          @roles << r
+    if not @project_role then
+      # select all roles where project owner is current member
+      @roles = []
+      Project.all.each do |p|
+        if p.owner == current_member then
+          p.project_roles.each do |r|
+            @roles << r
+          end
         end
       end
     end
@@ -39,7 +44,11 @@ class OffersController < ApplicationController
     @offer.description = offer_params[:description]
     respond_to do |format|
       if @offer.save
-        format.html { redirect_to project_dashboard_requests_path(@offer.project_role.project.id), notice: 'Offer was successfully created.' }
+        if @offer.project_role.project.owner == current_member then
+          format.html { redirect_to project_dashboard_requests_path(@offer.project_role.project.id), notice: 'Offer was successfully created.' }
+        else
+          format.html { redirect_to my_dashboard_proposals_path, notice: 'Offer was successfully created.' }
+        end
         format.json { render :show, status: :created, location: @offer }
       else
         format.html { render :new }
@@ -80,6 +89,13 @@ class OffersController < ApplicationController
     
     def set_receiver
       @receiver = Member.find(params[:receiver_id])
+    end
+
+    def set_project_role
+      @project_role = nil
+      if params[:project_role_id] then
+        @project_role = ProjectRole.find(params[:project_role_id])
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
