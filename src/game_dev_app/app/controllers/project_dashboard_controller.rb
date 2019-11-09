2b821fcab807af6774bdf86ae3c442933f0a9325
reez <zreye24@gmail.com>
2019-11-10 00:48:10 +1000

    classify requests by inbound/outbound

--- src/game_dev_app/app/controllers/project_dashboard_controller.rb
+++ src/game_dev_app/app/controllers/project_dashboard_controller.rb
@@ -12,17 +12,15 @@ class ProjectDashboardController < ApplicationController
   end
 
   def requests
-    # you ask someone if you can work on their project
-    @pending = Offer.where(
-      # commented code is my failed attempt at writing sql
-      # "sender_id = :current_member AND project.owner_id != :current_member", 
-      # {current_member: current_member.id}
-      sender: current_member,
-      response: nil,
-    )
-    @requests = []
-    @pending.each do |pend|
-      @requests << pend if pend.project_role.project.owner == current_member
+    # outbound - offers where sender and project owner is current user
+    @outbound_requests = []
+    Offer.where(sender: current_member, response: nil).each do |pend|
+      @outbound_requests << pend if pend.project_role.project == @project
+    end
+    # inbound - offers where receiver is current member and project is this project
+    @inbound_requests = []
+    Offer.where(receiver: current_member, response: nil).each do |pend|
+      @inbound_requests << pend if pend.project_role.project == @project
     end
   end
 
